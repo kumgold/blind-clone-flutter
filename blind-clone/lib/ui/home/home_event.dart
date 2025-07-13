aaf8a0a4a0666f5ffbe692158ec481b49263e52a
GOLD <123260423+kumgold@users.noreply.github.com>
2025-07-13 16:18:39 +0900

    Update home_event.dart

--- blind-clone/lib/ui/home/home_event.dart
+++ blind-clone/lib/ui/home/home_event.dart
@@ -1,21 +1,21 @@
-import 'package:blind_clone_flutter/data/post.dart';
-import 'package:equatable/equatable.dart';
+// import 'package:blind_clone_flutter/data/post.dart';
+// import 'package:equatable/equatable.dart';
 
-abstract class HomeEvent extends Equatable {
-  const HomeEvent();
+// abstract class HomeEvent extends Equatable {
+//   const HomeEvent();
 
-  @override
-  List<Object> get props => [];
-}
+//   @override
+//   List<Object> get props => [];
+// }
 
-class FetchPosts extends HomeEvent {}
+// class FetchPosts extends HomeEvent {}
 
-// 내부적으로 스트림에서 새로운 데이터가 왔을 때 사용할 이벤트
-class _HomeUpdated extends HomeEvent {
-  final List<Post> posts;
+// // 내부적으로 스트림에서 새로운 데이터가 왔을 때 사용할 이벤트
+// class _HomeUpdated extends HomeEvent {
+//   final List<Post> posts;
 
-  const _HomeUpdated(this.posts);
+//   const _HomeUpdated(this.posts);
 
-  @override
-  List<Object> get props => [posts];
-}
+//   @override
+//   List<Object> get props => [posts];
+// }
