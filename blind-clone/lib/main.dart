516342e37f841071c43e0d36685ac0fbb8cb17ef
GOLD <123260423+kumgold@users.noreply.github.com>
2025-07-13 15:34:57 +0900

    🛠️ init firebase
    - core
    - database

--- blind-clone/lib/main.dart
+++ blind-clone/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:blind_clone_flutter/ui/home/home_bloc.dart';
 import 'package:blind_clone_flutter/ui/home/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
