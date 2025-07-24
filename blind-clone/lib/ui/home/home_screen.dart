63ae2bb805e11e4097c02dcd3e28860597a9158f
GOLD <123260423+kumgold@users.noreply.github.com>
2025-07-24 21:28:57 +0900

    📝 refactor homs_screen
    - Floating button

--- blind-clone/lib/ui/home/home_screen.dart
+++ blind-clone/lib/ui/home/home_screen.dart
@@ -26,45 +26,52 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: BlocBuilder<HomeBloc, HomeState>(
-        builder: (context, state) {
-          if (state is HomeInitial) {
-            return Text('home initial');
-          }
-          if (state is HomeLoading) {
-            return Text('home loading');
-          }
-
-          if (state is HomeResult) {
-            if (state.posts.isEmpty) {
-              return const Center(child: Text('게시물이 없습니다.'));
+    return Scaffold(
+      body: Center(
+        child: BlocBuilder<HomeBloc, HomeState>(
+          builder: (context, state) {
+            if (state is HomeInitial) {
+              return Center();
             }
-            return RefreshIndicator(
-              onRefresh: _onRefresh,
-              child: ListView.builder(
-                itemCount: state.posts.length,
-                itemBuilder: (context, index) {
-                  final post = state.posts[index];
-                  return ListTile(
-                    title: Text(post.title),
-                    subtitle: Text(post.content),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              PostDetailScreen(postId: post.id),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            );
-          }
-          return Text('error state');
-        },
+
+            if (state is HomeLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (state is HomeResult) {
+              if (state.posts.isEmpty) {
+                return const Center(child: Text('게시물이 없습니다.'));
+              }
+              return RefreshIndicator(
+                onRefresh: _onRefresh,
+                child: ListView.builder(
+                  itemCount: state.posts.length,
+                  itemBuilder: (context, index) {
+                    final post = state.posts[index];
+                    return ListTile(
+                      title: Text(post.title),
+                      subtitle: Text(post.content),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                PostDetailScreen(postId: post.id),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              );
+            }
+            return Text('');
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: const Icon(Icons.post_add_rounded),
       ),
     );
   }
