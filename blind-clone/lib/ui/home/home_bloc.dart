4a6f24a7a96a88fc3e83dbb0a1223f1dc46921e3
JunghunKum <123260423+kumgold@users.noreply.github.com>
2025-08-06 22:37:08 +0900

    📝 refactor- fetch posts
    - 불필요한 로직 제거

--- blind-clone/lib/ui/home/home_bloc.dart
+++ blind-clone/lib/ui/home/home_bloc.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:blind_clone_flutter/data/post.dart';
 import 'package:blind_clone_flutter/data/post_repository.dart';
 import 'package:blind_clone_flutter/ui/home/home_state.dart';
 import 'package:equatable/equatable.dart';
@@ -15,16 +14,6 @@ abstract class HomeEvent extends Equatable {
 
 class FetchPosts extends HomeEvent {}
 
-// 내부적으로 스트림에서 새로운 데이터가 왔을 때 사용할 이벤트
-class _HomeUpdated extends HomeEvent {
-  final List<Post> posts;
-
-  const _HomeUpdated(this.posts);
-
-  @override
-  List<Object> get props => [posts];
-}
-
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final PostRepository _postRepository;
 
@@ -33,20 +22,15 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
       super(HomeLoading()) {
     // 초기 상태는 로딩
     on<FetchPosts>(_onFetchPosts);
-    on<_HomeUpdated>(_onPostsUpdated);
   }
 
   void _onFetchPosts(FetchPosts event, Emitter<HomeState> emit) async {
     try {
       final result = await _postRepository.getPosts();
-      add(_HomeUpdated(result));
+      emit(HomeResult(posts: result));
     } catch (e) {
-      add(_HomeUpdated([]));
+      emit(HomeResult(posts: []));
     }
-  }
-
-  void _onPostsUpdated(_HomeUpdated event, Emitter<HomeState> emit) {
-    emit(HomeResult(posts: event.posts));
   }
 
   @override
